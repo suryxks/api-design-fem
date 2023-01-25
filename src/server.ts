50219efc6f8632578d7bfc3d7eb194bcb0fbaa93
Surya <123004250@sastra.ac.in>
2023-01-25 18:30:23 +0530

    added routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from 'express',
-
+import express from 'express';
+import router from './router';
+import morgan from 'morgan'
 const app = express();
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req,res) => {
     console.log('hello from express')
     res.status(200)
-    res.json({ message: 'hello' });
+    res.json({ message: 'hello' })
 })
-
+app.use('/api',router)
 export default app;
