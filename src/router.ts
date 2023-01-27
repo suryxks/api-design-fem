d8ba47bba3f59fe3ebaf71d0c528bdfd2730bbcc
Surya <123004250@sastra.ac.in>
2023-01-27 07:54:18 +0530

    feat: added validators

--- src/router.ts
+++ src/router.ts
@@ -1,34 +1,64 @@
-import { Router } from 'express';
+import { handleInputErrors } from "./modules/middleware";
+import { Router } from "express";
+import { body, oneOf } from "express-validator";
 
 const router = Router();
 /**
  * Product
  */
-router.get('/product', (req, res) => {
-    res.json({message:'hi'})
- })
-router.get('/product/:id', () => { })
-router.put('/product/:id', () => { })
-router.post('/product', () => { })
-router.delete('/product/:id', () => { })
+router.get("/product", (req, res) => {
+  res.json({ message: "hi" });
+});
+router.get("/product/:id", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post("/product", body("name").isString(), handleInputErrors, () => {});
+router.delete("/product/:id", () => {});
 
 /**
  * Update
  */
-router.get('/update', () => { })
-router.get('/update/:id', () => { })
-router.put('/update/:id', () => { })
-router.post('/update', () => { })
-router.delete('/update/:id', () => { })
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  oneOf([body("IN_PROGRESS"), body("SHIPPED"), body("DEPRECATED")]),
+  body("version").optional(),
+  (req, res) => {}
+);
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  () => {}
+);
+router.delete("/update/:id", () => {});
 
 /**
  * Update Point
  */
 
-router.get('/updatepoint', () => { })
-router.get('/updatepoint/:id', () => { })
-router.put('/updatepoint/:id', () => { })
-router.post('/updatepoint', () => { })
-router.delete('/updatepoint/:id', () => { })
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "/updatepoint",
+  body("name").exists().isString(),
+  body("description").exists().isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
+router.delete("/updatepoint/:id", () => {});
 
 export default router;
